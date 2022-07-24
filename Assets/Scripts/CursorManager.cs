344a15e7830f6e499f41f55c150a59504c0acf52
postive12 <postive12@gmail.com>
2022-07-24 17:00:44 +0900

    Cursor and Scroll view and Layout Practice

---
+++ Assets/Scripts/CursorManager.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorManager : MonoBehaviour
+{
+    public static CursorManager current = null;
+    [SerializeField] private RectTransform _mouseCursor;
+    private void Start()
+    {
+        if (current != null) {
+            Destroy(current.gameObject);
+        }
+        Cursor.visible = false;
+        current = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Update()
+    {
+        _mouseCursor.position = Input.mousePosition + new Vector3(30,-30,0);
+    }
+}
---
+++ Assets/Scripts/EpisodeButton.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EpisodeButton : MonoBehaviour
+{
+    [SerializeField] private int EpisodeNumber = 1;
+
+    public void OnClick()
+    {
+        Debug.Log($"{EpisodeNumber:0,0}" + " 에피소드가 선택되었습니다.");
+    }
+}

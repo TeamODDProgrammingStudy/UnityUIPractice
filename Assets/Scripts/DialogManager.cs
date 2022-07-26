08092cc2a352db6dc4ddf130775988fa49da39b7
postive12 <postive12@gmail.com>
2022-07-26 22:28:04 +0900

    스터디 진행 외의 스킵 모드 예제

--- Assets/Scripts/DialogManager.cs
+++ Assets/Scripts/DialogManager.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
@@ -10,13 +7,11 @@ public class DialogManager : MonoBehaviour
     [SerializeField] private Text _dialog;
     [SerializeField] private string[] _paragraph;
     [SerializeField] private bool _isPaused = false;
-
     private int _counter = 0;
     private void Start()
     {
         LoadParagraph();
     }
-
     private void Update()
     {
         if (_isPaused) {
@@ -32,20 +27,49 @@ public class DialogManager : MonoBehaviour
                 LoadParagraph();
             }
         }
+        //SkipMode();//스킵 모드 함수
     }
-
+    /*//스킵모드 코드
+    private float _startTime = 0; // 스킵 입력 시작 변수
+    private bool _startSkipTimer = false; // 스킵 타이머 시작 여부를 저장하는 변수
+    private float _pastSkipTime = 0f; // 이전 출력 시기를 저장하는 변수
+    private float _skipRatio = 0.5f; // 스킵 주기 변수
+    private void SkipMode()
+    {
+        //마우스를 꾹 누르고 있는 입력을 받음
+        if (Input.GetMouseButton(0)) {
+            //입력시 스킵 타이머가 동작하지 않았을 경우 현재 시간을 변수에 저장하고 타이머 실행 여부를 True로 바꿈
+            if (!_startSkipTimer) {
+                _startTime = Time.time;
+                _startSkipTimer = true;
+            }
+        }
+        else {
+            //입력이 없으면 타이머 초기화
+            _startTime = 0;
+            _startSkipTimer = false;
+        }
+        //스킵 타이머가 작동하고 있었고 그 주기가 1.5초를 초과하면 스킵 실행
+        if (_startSkipTimer &&Time.time - _startTime > 1.5f) {
+            //현재 시간에 이전 스킵 시간을 뺀 값이 주기 값보다 크면 문장 출력 및 이전 스킵 시간 저장
+            if (Time.time - _pastSkipTime > _skipRatio) {
+                LoadParagraph();
+                _pastSkipTime = Time.time;
+            }
+            
+        }
+    }
+    */
     public void LoadParagraph()
     {
         _dialog.text = _paragraph[_counter];
         _counter++;
         _counter %= _paragraph.Length;
     }
-
     public void DialogPause()
     {
         _isPaused = true;
     }
-
     public void DialogStart()
     {
         _isPaused = false;
